78c3e47f8ec3f0cdd4b3f9205f5a0242cdd7834f
mtenyg24 <m.gatluak@alustudent.com>
2024-01-15 23:31:16 +0300

    Slice Me Up

---
+++ math/linear_algebra/0-slice_me_up.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+arr =  [9, 8, 2, 3, 9, 4, 1, 0, 3]
+
+arr1 = arr[0:2] # should be the first two numbers of the array
+
+arr2 = arr[ -5:] # the last five numbers of the array 
+
+arr3 = arr[1: 6] # the 2nd through 6th numbers of the array
+
+print("The first two numbers of the array are: {}". format(arr1))
+
+print("The last five numbers of the array are: {}". format(arr2))
+
+print("The 2nd through 6th numbers of the array are: {}". format(arr3))
