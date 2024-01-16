845841203e9862668be5f69bd9576a15dbd40305
mtenyg24 <m.gatluak@alustudent.com>
2024-01-16 15:29:13 +0300

    Match Dimensions

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,9 +4,9 @@
 
 def cat_matrices(mat1, mat2, axis=0):
     '''This function concatenates two matrices along a specific axis'''
-    if axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         return mat1 + mat2
-    elif axis == 1:
+    elif axis == 1 and len(mat1) == len(mat2):
         return [mat1[i] + mat2[i] for i in range(len(mat1))]
     else:
         return None
