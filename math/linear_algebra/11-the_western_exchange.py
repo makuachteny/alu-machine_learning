6a715d342f62353f28e8ed29fd9dc1b2a5a7ffca
mtenyg24 <m.gatluak@alustudent.com>
2024-01-16 16:54:50 +0300

    Syntax error corrected!

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,9 +1,7 @@
-import numpy as np
 #!/usr/bin/env python3
 '''This function transposes matrixes'''
 
 
 def np_transpose(matrix):
     '''This function transposes the matrix'''
-    transposed = np.transpose(matrix)
-    return transposed
+    return matrix.transpose()
