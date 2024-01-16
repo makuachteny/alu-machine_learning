9e3faa9839b7d4ad11e83b96ff4ba005da797462
mtenyg24 <m.gatluak@alustudent.com>
2024-01-16 15:18:29 +0300

    Syntax fix

--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''This function concatenates two arrays, assuming that the arrays are lists of ints/floats'''
+'''This function concatenates two arrays of integers/floats'''
 
 
 def cat_arrays(arr1, arr2):
     '''This function concatenates two arrays'''
     if len(arr1) == len(arr2):
-        return [arr1[i] + arr2[i] for i in range(len(arr1))]
+        return arr1 + arr2
     else:
         return None
