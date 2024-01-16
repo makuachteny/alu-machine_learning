6a50cd902f1b135655c12e5a390a02c7ef3d1c3b
mtenyg24 <m.gatluak@alustudent.com>
2024-01-16 15:56:14 +0300

    Align continuation

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -12,7 +12,7 @@ def mat_mul(mat1, mat2):
         return [
             [
                 sum([mat1[i][k] * mat2[k][j] for k in range(len(mat1[0]))])
-                 for j in range(len(mat2[0]))
+                for j in range(len(mat2[0]))
                  ] for i in range(len(mat1))
             ]
     else:
