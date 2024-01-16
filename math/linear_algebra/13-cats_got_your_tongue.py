70f157a6dd7914e95f03e3409c773a455c06ca78
mtenyg24 <m.gatluak@alustudent.com>
2024-01-16 19:57:48 +0300

    Docstring!

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 
-'''This function concatenates two matrices along a specific axis'''
+'''This module provides a function for concatinating matrices'''
 
 
 def np_cat(mat1, mat2, axis=0):
