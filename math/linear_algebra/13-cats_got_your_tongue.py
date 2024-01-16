bd5fc41fe3fd103e6a7dc3c37b8808e0e5371200
mtenyg24 <m.gatluak@alustudent.com>
2024-01-16 19:49:04 +0300

    Trailing space removed

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import numpy as np
-'''This function concatenates two matrices along a specific axis using Numpy library'''
+'''This function concatenates two matrices along a specific axis'''
 
 
 def np_cat(mat1, mat2, axis=0):
-    '''This function concatenates two matrices along a specific axis using Numpy library'''
+    '''This function concatenates two matrices along a specific axis'''
     return np.concatenate((mat1, mat2), axis)
