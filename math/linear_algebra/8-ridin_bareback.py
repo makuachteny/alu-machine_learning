891c9bb1cdc1f21734ce49f097b3d04c0254b642
mtenyg24 <m.gatluak@alustudent.com>
2024-01-16 15:46:12 +0300

    Syntax error fixed

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -8,7 +8,7 @@ def mat_mul(mat1, mat2):
     '''
     This function performs matrix multiplication
     '''
-    if len(mat1[0]) == len(mat2) and len(mat1) == len(mat2[0]):
+    if len(mat1[0]) == len(mat2):
         return [[sum([mat1[i][k] * mat2[k][j] for k in range(len(mat1[0]))])
                  for j in range(len(mat2[0]))] for i in range(len(mat1))]
     else:
