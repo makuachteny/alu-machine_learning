8956abf06a261f1dcc7df939e075b04a3895b8cf
mtenyg24 <m.gatluak@alustudent.com>
2024-01-16 16:48:10 +0300

    Transpose without  loops

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,12 +1,9 @@
+import numpy as np
 #!/usr/bin/env python3
 '''This function transposes matrixes'''
 
 
 def np_transpose(matrix):
     '''This function transposes the matrix'''
-    transposed = []
-    for i in range(len(matrix[0])):
-        transposed.append([])
-        for j in range(len(matrix)):
-            transposed[i].append(matrix[j][i])
+    transposed = np.transpose(matrix)
     return transposed
