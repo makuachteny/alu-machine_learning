0e4b26d4fed79eb5699f6a8a945c83277f4ee68c
mtenyg24 <m.gatluak@alustudent.com>
2024-02-03 21:31:25 +0300

    very hot fix

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -11,7 +11,3 @@ def poly_derivative(poly):
     if not isinstance(poly, list) or len(poly) <= 1:
         return [0]
     return [poly[i] * i for i in range(1, len(poly))]
-
-
-poly = [5, 3, 0, 1]
-print(poly_derivative(poly))
