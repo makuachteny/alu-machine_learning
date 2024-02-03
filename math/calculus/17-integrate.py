561208648c8e5354e442e00c9f698a177bc9a5bc
mtenyg24 <m.gatluak@alustudent.com>
2024-02-03 11:43:55 +0300

    check entire integral list

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,18 +4,27 @@
 
 def poly_integral(poly, C=0):
     """
-    function that integrate a polynomialcoefficients
+    Function to integrate a polynomial's coefficients.
     """
-    if not isinstance(poly, list) or len(poly) < 1 or \
-            not isinstance(C, (int, float)):
+    if not isinstance(poly, list) or len(poly) < 1 or not isinstance(C, (int, float)):
         return None
+
     if isinstance(C, float) and C.is_integer():
         C = int(C)
+
     integral = [C]
+
     for power, coefficient in enumerate(poly):
         new_coefficient = coefficient // (power + 1) if coefficient % (
             power + 1) == 0 else coefficient / (power + 1)
         integral.append(new_coefficient)
-    while integral and integral[-1] == 0:
+
+    # Check if the entire integral list is [0]
+    if integral == [0]:
+        return integral
+
+    # Remove trailing zeros
+    while integral[-1] == 0:
         integral.pop()
+
     return integral
