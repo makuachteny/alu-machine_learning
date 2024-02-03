331115f4348a3c25a5754d1d841c3987bdde9cee
mtenyg24 <m.gatluak@alustudent.com>
2024-02-03 10:53:59 +0300

    Use closed-form formula

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
-''' This module calculates the total sum of a summation'''
+'''
+    This module defines a function that calculates the summation
+    of numbers from 1 to n
+'''
 
 
 def summation_i_squared(n):
-    '''This function calculates the total sum of a summation'''
-    # Check if n is valid number
-    if not isinstance(n, int) or n < 1:
+    '''
+    calculates the summation
+    squares of numbers from 1 to n
+    '''
+    if type(n) is not int or n < 1:
         return None
-    elif n == 1:
-        return 1
-    else:
-        return n**2 + summation_i_squared(n - 1)
-
-
-# Example usage:
-result = summation_i_squared(5)
-print(result)
+    summation = (n * (n + 1) * ((2 * n) + 1)) / 6
+    return int(summation)
