aaa71fc58491a7e54c7f5e9b3503e85cc433c055
mtenyg24 <m.gatluak@alustudent.com>
2024-02-16 12:12:50 +0300

    hot fix

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 ''' Convolution on grayscale images with custom padding '''
 
-
 import numpy as np
 
 
@@ -11,23 +10,31 @@ def convolve_grayscale_padding(images, kernel, padding):
     m, h, w = images.shape
     kh, kw = kernel.shape
 
-    # Calculate the needed padding
-    ph = max((kh - 1) // 2, kh // 2)
-    pw = max((kw - 1) // 2, kw // 2)
+    # Add some padding based on the specified padding mode
+    if padding == 'same':
+        ph = max((kh - 1), 0)
+        pw = max((kw - 1), 0)
+    elif padding == 'valid':
+        ph = 0
+        pw = 0
+    else:
+        ph, pw = padding
 
     # calculate the padded images, the images should be padded with zeros
     padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
 
-    # calculate the padded height and width of the convolved images
+    # calculate the output height and width of the convolved images
     padded_h = h + 2 * ph
     padded_w = w + 2 * pw
+    ch = padded_h - kh + 1
+    cw = padded_w - kw + 1
 
     # calculate the output height and width of the convolved image
-    convolved_images = np.zeros(((m, padded_h, padded_w)))
+    convolved_images = np.zeros((m, ch, cw))
 
-# Perform convolution on each pixel
-    for i in range(padded_h - kh + 1):
-        for j in range(padded_w - kw + 1):
+    # Perform convolution on each pixel
+    for i in range(ch):
+        for j in range(cw):
             # Extract the region of interest (ROI) from the padded images
             roi = padded_images[:, i:i+kh, j:j+kw]
             # Apply convolution by element-wise multiplication and summation
