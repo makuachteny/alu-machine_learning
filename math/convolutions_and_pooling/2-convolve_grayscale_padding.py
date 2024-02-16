4b6762c477575a76de6df1b63bfa2929ba597cfc
mtenyg24 <m.gatluak@alustudent.com>
2024-02-16 11:32:17 +0300

    correct syntax error

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-''' This module performs a convolution on grayscale images with custom padding '''
+''' Convolution on grayscale images with custom padding '''
 
 
 import numpy as np
 
+
 def convolve_grayscale_padding(images, kernel, padding):
-    ''' Function performs a convolution on grayscale images with custom padding '''
+    ''' Function performs covolves images with custom padding '''
 
     m, h, w = images.shape
     kh, kw = kernel.shape
@@ -15,17 +16,18 @@ def convolve_grayscale_padding(images, kernel, padding):
     pw = max((kw - 1) // 2, kw // 2)
 
     # calculate the padded images, the images should be padded with zeros
-
     padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
-    
-    ph_h = h
-    pw_w = w
+
+    # calculate the padded height and width of the convolved images
+    padded_h = h + 2 * ph
+    padded_w = w + 2 * pw
+
     # calculate the output height and width of the convolved image
-    convolved_images = np.zeros(((m, ph_h, pw_w)))
+    convolved_images = np.zeros(((m, padded_h, padded_w)))
 
     # Perform convolution on each pixel
-    for i in range(ph_h):
-        for j in range(pw_w):
+    for i in range(padded_h):
+        for j in range(padded_w):
             convolved_images[:, i, j] = np.sum(
                 padded_images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
             )
