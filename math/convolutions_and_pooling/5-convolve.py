6c6d8a1f7f6720bff69c415e9236a4253cdda8dc
mtenyg24 <m.gatluak@alustudent.com>
2024-02-19 09:50:17 +0300

    multiple kernel

---
+++ math/convolutions_and_pooling/5-convolve.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+'''This module performs convolution on images with multiple kernels'''
+
+import numpy as np
+
+
+def convolve(images, kernels, padding='same', stride=(1, 1)):
+    ''' This function performs convolution on images with multiple kernels '''
+    m, h, w, c = images.shape
+    kh, kw, c, nc = kernels.shape
+    sh, sw = stride
+
+    # Add padding according to the specified padding scheme/model
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2
+        pw = ((w - 1) * sw + kw - w) // 2
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    padded_images = np.pad(
+        images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+
+    ch = (h + 2 * ph - kh) // sh + 1
+    cw = (w + 2 * pw - kw) // sw + 1
+
+    convolved_images = np.zeros((m, ch, cw, nc))
+
+    for i in range(ch):
+        for j in range(cw):
+            for k in range(nc):
+                convolved_images[:, i, j, k] = np.sum(
+                    padded_images[:, i * sh:i * sh + kh, j *
+                                  sw:j * sw + kw, :] * kernels[:, :, :, k],
+                    axis=(1, 2, 3)
+                )
+
+    return convolved_images
---
+++ math/convolutions_and_pooling/5-main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+convolve = __import__('5-convolve').convolve
+
+
+if __name__ == '__main__':
+
+    dataset = np.load(
+        'C:\\Users\\Lenovo\\Documents\\2. SE\ALU\\alu-machine_learning\math\convolutions_and_pooling\\dataset\\animals_1.npz')
+    images = dataset['data']
+    print(images.shape)
+    kernels = np.array([[[[0, 1, 1], [0, 1, 1], [0, 1, 1]], [[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]], [[0, -1, 1], [0, -1, 1], [0, -1, 1]]],
+                       [[[-1, 1, 0], [-1, 1, 0], [-1, 1, 0]], [[5, 0, 0], [5, 0, 0],
+                                                               [5, 0, 0]], [[-1, -1, 0], [-1, -1, 0], [-1, -1, 0]]],
+                       [[[0, 1, -1], [0, 1, -1], [0, 1, -1]], [[-1, 0, -1], [-1, 0, -1], [-1, 0, -1]], [[0, -1, -1], [0, -1, -1], [0, -1, -1]]]])
+
+    images_conv = convolve(images, kernels, padding='valid')
+    print(images_conv.shape)
+
+
+    fig, axes = plt.subplots(1, 2)
+    
+    # Display the original image
+    axes[0].imshow(images[0])
+    axes[0].set_title('Original Image')
+    
+    # Display the convolved image
+    axes[1].imshow(images_conv[0])
+    axes[1].set_title('Convolved Image')
+    
+    plt.show()
+    
