1ade6f4a137043d7da0460bfce8c90614b4e0cdb
mtenyg24 <m.gatluak@alustudent.com>
2024-02-21 10:20:30 +0300

    hot fix

--- math/convolutions_and_pooling/5-convolve.py
+++ math/convolutions_and_pooling/5-convolve.py
@@ -6,34 +6,30 @@ import numpy as np
 
 def convolve(images, kernels, padding='same', stride=(1, 1)):
     ''' This function performs convolution on images with multiple kernels '''
-    m, h, w, c = images.shape
-    kh, kw, c, nc = kernels.shape
+    m, height, width, c = images.shape
+    kh, kw, kc, nc = kernels.shape
     sh, sw = stride
-
-    # Add padding according to the specified padding scheme/model
-    if padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
-    elif padding == 'valid':
-        ph, pw = 0, 0
+    if padding is 'same':
+        ph = ((((height - 1) * sh) + kh - height) // 2) + 1
+        pw = ((((width - 1) * sw) + kw - width) // 2) + 1
+    elif padding is 'valid':
+        ph = 0
+        pw = 0
     else:
         ph, pw = padding
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                    'constant', constant_values=0)
+    ch = ((height + (2 * ph) - kh) // sh) + 1
+    cw = ((width + (2 * pw) - kw) // sw) + 1
+    convoluted = np.zeros((m, ch, cw, nc))
 
-    padded_images = np.pad(
-        images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+    for index in range(nc):
+        kernel_index = kernels[:, :, :, index]
+        for i, h in enumerate(range(0, (height + (2 * ph) - kh + 1), sh)):
+            for j, w in enumerate(range(0, (width + (2 * pw) - kw + 1), sw)):
+                output = np.sum(images[:, h: h + kh, w: w + kw, :]
+                                * kernel_index, axis=(1, 2, 3))
+                convoluted[:, i, j, index] = output
 
-    ch = (h + 2 * ph - kh) // sh + 1
-    cw = (w + 2 * pw - kw) // sw + 1
+    return convoluted
 
-    convolved_images = np.zeros((m, ch, cw, nc))
-
-    for i in range(ch):
-        for j in range(cw):
-            for k in range(nc):
-                convolved_images[:, i, j, k] = np.sum(
-                    padded_images[:, i * sh:i * sh + kh, j *
-                                  sw:j * sw + kw, :] * kernels[:, :, :, k],
-                    axis=(1, 2, 3)
-                )
-
-    return convolved_images
