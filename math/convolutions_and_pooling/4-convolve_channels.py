70e0fdd74e777d70463313157a25b98764cf8588
mtenyg24 <m.gatluak@alustudent.com>
2024-02-21 10:17:57 +0300

    valid padding corrected

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -8,39 +8,27 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     ''' Performs convolutions on images channels '''
 
     m, h, w, c = images.shape
-    kh, kw, cc = kernel.shape
+    kh, kw, kc = kernel.shape
     sh, sw = stride
-
-    # Calculate padding
     if padding == 'same':
-        ph = max((h - 1) * sh + kh - h, 0) // 2
-        pw = max((w - 1) * sw + kw - w, 0) // 2
+        ph = ((((h - 1) * sh) + kh - h) // 2) + 1
+        pw = ((((w - 1) * sw) + kw - w) // 2) + 1
     elif padding == 'valid':
-        ph, pw = 0, 0
+        ph = 0
+        pw = 0
     else:
         ph, pw = padding
+    images = np.pad(images, ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                    'constant', constant_values=0)
+    ch = ((h + (2 * ph) - kh) // sh) + 1
+    cw = ((w + (2 * pw) - kw) // sw) + 1
+    convoluted = np.zeros((m, ch, cw))
 
-    # Pad images
-    padded_images = np.pad(
-        images, ((0, 0), (ph, ph), (pw, pw), (0, 0)), 'constant')
+    for i, h in enumerate(range(0, (h + (2 * ph) - kh + 1), sh)):
+        for j, w in enumerate(range(0, (w + (2 * pw) - kw + 1), sw)):
+            output = np.sum(images[:, h: h + kh, w: w + kw, :] * kernel,
+                            axis=(1, 2, 3))
+            convoluted[:, i, j] = output
 
-    # Calculate output dimensions
-    if padding == 'valid':
-        ch = (h - kh + 2 * ph) // sh + 1
-        cw = (w - kw + 2 * pw) // sw + 1
-    else:
-        ch = (h + 2 * ph - kh) // sh + 1
-        cw = (w + 2 * pw - kw) // sw + 1
+    return convoluted
 
-    # Initialize convolved images
-    convolved_images = np.zeros((m, ch, cw))
-
-    # Perform convolution
-    for i in range(ch):
-        for j in range(cw):
-            convolved_images[:, i, j] = np.sum(
-                padded_images[:, i*sh:i*sh+kh, j*sw:j*sw+kw, :] * kernel,
-                axis=(1, 2, 3)
-            )
-
-    return convolved_images
