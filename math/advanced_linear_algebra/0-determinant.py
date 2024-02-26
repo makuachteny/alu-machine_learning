ff55872af84a9010556c57c8bb3fee49cb73cec9
mtenyg24 <m.gatluak@alustudent.com>
2024-02-26 08:41:35 +0300

    hot fix

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -7,30 +7,29 @@ This module contains a function that calculates the determinant of a matrix.
 def determinant(matrix):
     '''This function calculates the determinant of a matrix.'''
 
-    # Check if the matrix is a list of lists
-    if not all(isinstance(row, list) for row in matrix):
-        raise TypeError('Matrix must be a list of lists')
-
-    # Check if the matrix is a 0x0 matrix
-    if len(matrix) == 0 or not isinstance(matix, list):
-        raise TypeError('Matrix must be a non-empty list of lists')
-    
-    # Checks if the matrix is 1x1 with no elements
+    # Checks if the matrix is a list of lists
+    if not all(type(row) == list for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 0 or type(matrix) != list:
+        raise TypeError("matrix must be a list of lists")
     if matrix == [[]]:
         return 1
-
-    # Check if matrix is square
-    if not all(len(row) == len(matrix) for row in matrix):
-        raise ValueError('matrix must be a square matrix')
-
-    # Calculate the determinant of the matrix
+    if not all(len(r) == len(matrix) for r in matrix):
+        raise ValueError("matrix must be a square matrix")
     if len(matrix) == 1:
         return matrix[0][0]
-    elif len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    else:
-        det = 0
-        for i in range(len(matrix)):
-            minor = [row[:i] + row[i+1:] for row in matrix[1:]]
-            det += matrix[0][i] * ((-1) ** i) * determinant(minor)
-        return det
+    if len(matrix) == 2:
+        x = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+        return x
+    det = 0
+    for x, num in enumerate(matrix):
+        temp = []
+        P = matrix[0][x]
+        for row in matrix[1:]:
+            new = []
+            for j in range(len(matrix)):
+                if j != x:
+                    new.append(row[j])
+            temp.append(new)
+        det += P * determinant(temp) * (-1) ** x
+    return det
