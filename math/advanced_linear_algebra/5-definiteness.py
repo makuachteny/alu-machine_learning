1c79cb1e54409b69861e5e7551dbf39dad5abd00
mtenyg24 <m.gatluak@alustudent.com>
2024-02-26 09:02:59 +0300

    definiteness

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,37 +1,26 @@
 #!/usr/bin/env python3
-
-'''
-This module contains a function.
-It calculates the determinant of a square matrix.
-'''
-
-
+""" This module determines the definiteness of a matrix """
 import numpy as np
 
-def determinant(matrix):
-    '''This function calculates the determinant of a matrix'''
-    
-    # check if the matrix is a list of lists
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a list of lists")
-    
-    # Check if matrix is square
-    if len(matrix) != len(matrix[0]):
-        raise ValueError("matrix must be a square matrix")
-    
-    # Check if the matrix is a 0x0 matrix
-    if len(matrix) == 0:
-        return 1
-    
-    # Calculate the determinant of the matrix
-    if len(matrix) == 1:
-        return matrix[0][0]
-    elif len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+def definiteness(matrix):
+    """ definitness"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    my_len = matrix.shape[0]
+    if len(matrix.shape) != 2 or my_len != matrix.shape[1]:
+        return None
+    transpose = np.transpose(matrix)
+    if not np.array_equal(transpose, matrix):
+        return None
+    eVals, eVecs = np.linalg.eig(matrix)
+    if all(eVals > 0):
+        return "Positive definite"
+    if all(eVals >= 0):
+        return "Positive semi-definite"
+    if all(eVals < 0):
+        return "Negative definite"
+    if all(eVals <= 0):
+        return "Negative semi-definite"
     else:
-        det = 0
-        for i in range(len(matrix)):
-            # Calculate the minor for each element in the first row
-            minor = [row[:i] + row[i+1:] for row in matrix[1:]] 
-            det += matrix[0][i] * ((-1) ** i) * determinant(minor)
-        return det
+        return 'Indefinite'
