f3044a33ed9f6e9bc892eaff7822849fd94cb6b0
mtenyg24 <m.gatluak@alustudent.com>
2024-02-26 08:51:07 +0300

    documentation

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -10,14 +10,23 @@ def determinant(matrix):
     # Checks if the matrix is a list of lists
     if not all(type(row) == list for row in matrix):
         raise TypeError("matrix must be a list of lists")
+    # Checks if matrix is empty or not a list of lists
+    
     if len(matrix) == 0 or type(matrix) != list:
         raise TypeError("matrix must be a list of lists")
+    
+    # Checks if a matrix is a single empty list
     if matrix == [[]]:
         return 1
+    
+    # Checks if every row in the matrix has the same length as the matrix
     if not all(len(r) == len(matrix) for r in matrix):
         raise ValueError("matrix must be a square matrix")
+    # Checks if a matrix is a 1x1 matrix 
     if len(matrix) == 1:
         return matrix[0][0]
+    
+    # Checks if the matrix is a 2x2 matrix
     if len(matrix) == 2:
         x = (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
         return x
