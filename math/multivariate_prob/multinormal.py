c7c3fb599f7680b1fd16b2b772338c3c9860490b
mtenyg24 <m.gatluak@alustudent.com>
2024-03-08 14:03:24 +0300

    syntax fix

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,64 +1,60 @@
 #!/usr/bin/env python3
-
 """
-This module contains a class that represents
-a Multivariate Normal distribution.
+Multivariate Probability
 """
 import numpy as np
 
 
-class MultiNormal:
+class MultiNormal():
     """
-    class initialization
+    Multivariate Normal distribution
     """
 
     def __init__(self, data):
         """
-        class constructor
-        data - numpy.ndarray - shape (d, n)-
-        containing the data set:
-        n - int - number of data points
-        d - int - number of dimensions in each data point
+        Constructor method
+
+        data is a numpy.ndarray of shape (d, n) containing the data set:
+        n is the number of data points
+        d is the number of dimensions in each data point
+        If data is not a 2D numpy.ndarray, raise a TypeError with the message
+        data must be a 2D numpy.ndarray
+        If n is less than 2, raise a ValueError with the message data must
+        contain multiple data points
         """
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
-            raise TypeError('data must be a 2D numpy.ndarray')
-
+            raise TypeError("data must be a 2D numpy.ndarray")
         if data.shape[1] < 2:
-            raise ValueError('data must contain multiple data points')
-
-        mean = np.mean(data, axis=1).reshape(data.shape[0], 1)
-        self.mean = mean
-        cov = np.matmul(data - mean, data.T) / (data.shape[1] - 1)
-        self.cov = cov
+            raise ValueError("data must contain multiple data points")
+        d = data.shape[0]
+        n = data.shape[1]
+        self.mean = np.mean(data, axis=1).reshape(d, 1)
+        cov = data - self.mean
+        self.cov = np.dot(cov, cov.T) / (n - 1)
 
     def pdf(self, x):
         """
-        calculate PDF at a data point
-        x - numpy.ndarray - shape (d, 1) - containing the data point
-        d - int - number of dimensions of the Multinomial instance
+        Calculates the PDF at a data point
+
+        x is a numpy.ndarray of shape (d, 1) containing the data point whose
+        PDF should be calculated
+        d is the number of dimensions of the Multinomial instance
+
+        If x is not a numpy.ndarray, raise a TypeError with the message x must
+        be a numpy.ndarray
+        If x is not of shape (d, 1), raise a ValueError with the message x
+        must have the shape ({d}, 1)
+
+        Returns the value of the PDF
         """
-
-        if not isinstance(x, np.ndarray):
-            raise TypeError('x must be a numpy.ndarray')
-
-        if x.shape != (self.mean.shape[0], 1):
-            raise ValueError(
-                'x must have the shape ({:d}, 1)'.
-                format(self.mean.shape[0])
-            )
-
-        # calculate PDF
-        # print(self.cov.shape)
         d = self.cov.shape[0]
-        x_m = x - self.mean
-        cov_inv = np.linalg.inv(self.cov)
-        det = np.linalg.det(self.cov)
-        # divide the formulae to two
-        # print(np.pi)
-        pi = 3.1415926536
-        prefactor = 1.0 / (np.sqrt((2 * pi) ** d * det))
-        # exponent = -0.5 * np.dot(np.dot(x_m.T, cov_inv), x_m)
-        exponent = -0.5 * np.matmul(np.matmul(x_m.T, cov_inv), x_m)
-        pdf = prefactor * np.exp(exponent)
-        # print(pdf)
-        return pdf[0][0]
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        if (len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+
+        x_hat = x - self.mean
+        pdf = (1 / (np.sqrt((2 * np.pi)**d * np.linalg.det(self.cov)))
+               * np.exp(-(np.linalg.solve(self.cov, x_hat).T.dot(x_hat)) / 2))
+
+        return float(pdf)
