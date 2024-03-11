53db0c8cde55c0e09ca4f2d53b12e32e1b5a8c1b
mtenyg24 <m.gatluak@alustudent.com>
2024-03-11 13:31:33 +0300

    import math

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,20 +1,22 @@
-#!/usr/bin env python3
-
-"""This module calculates the likelihood of data given a model"""
-
+#!/usr/bin/env python3
+"""BAYESIAN PROBAB"""
 import numpy as np
+from math import factorial as f
 
 
-def likelihood(x, n, p):
-    """Calculates the likelihood of obtaining the data
-    x: number of patients that develop severe side effects
-    n: total number of patients observed
-    p: is a 1D numpy.ndarray containing the various hypothetical probabilities of developing severe side effects
-    """
+def likelihood(x, n, P):
+    """calculates the likelihood of data various hypothetical probabilities"""
     if not isinstance(n, int) or n <= 0:
-        raise ValueError("n must be a positive integer")
+        raise ValueError('n must be a positive integer')
     if not isinstance(x, int) or x < 0:
-        raise ValueError("X must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            'x must be an integer that is greater than or equal to 0')
     if x > n:
-        raise ValueError("X cannot be greater than n")
-    if not isinstance(P, np.ndarray) 
+        raise ValueError('x cannot be greater than n')
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not ((0 <= P).all() and (P <= 1).all()):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    combinaison = f(n) / (f(x) * f(n - x))
+    R = combinaison * np.power(P, x) * (np.power((1 - P), (n - x)))
+    return R
