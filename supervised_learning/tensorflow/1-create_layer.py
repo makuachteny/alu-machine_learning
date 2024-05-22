f07a90c7a6b25efce0e1d635fbe03045060ac207
mtenyg24 <m.gatluak@alustudent.com>
2024-05-22 09:50:35 +0300

    line too long

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -5,14 +5,7 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-    """
-    Creates a dense layer using the previous layer with He et al. initialization.
-    Parameters: 
-    - prev: The output tensor from the previous layer
-    - n: The number of nodes in the layer to create
-    - activation: The activation function that the layer should use
-    Returns: The output tensor of the layer
-    """
+    """Creates a dense layer using previous layer."""
 
     # Initialize weights using He et al. initializer
     init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
