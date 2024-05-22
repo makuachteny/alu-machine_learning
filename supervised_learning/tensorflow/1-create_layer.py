de3a956ea994abfe27f938b753d3009a2543ae02
mtenyg24 <m.gatluak@alustudent.com>
2024-05-22 13:27:27 +0300

    Initialize the weights

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -8,10 +8,10 @@ def create_layer(prev, n, activation):
     """Creates a dense layer using previous layer."""
 
     # Initialize weights using He et al. initializer
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    W_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 
     # Create the dense layer
-    layer = tf.layers.dense(input=prev, units=n, activation=activation,
-                            kernel_initializer=init, name='layer')
+    layer = tf.layers.Dense(input=prev, units=n, activation=activation,
+                            kernel_initializer=W_init, name='layer')
 
-    return layer
+    return layer(prev)
