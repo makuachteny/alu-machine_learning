ca556e94932975c1de880ec53aa52ee1e5982df9
mtenyg24 <m.gatluak@alustudent.com>
2024-05-22 14:50:23 +0300

    debug

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-"""Module defines a function that creates a layer"""
-
+""" create layer """
 import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-    """Creates a dense layer using previous layer."""
-
-    # Initialize weights using He et al. initializer
-    W_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-
-    # Create the dense layer
-    layer = tf.layers.Dense(input=prev, units=n, activation=activation,
-                            kernel_initializer=W_init, name='layer')
-
-    return layer(prev)
+    """ create layer"""
+    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    L = tf.layers.Dense(units=n, activation=activation,
+                        kernel_initializer=W, name="layer")
+    return L(prev)
