68ede37516185a9b0a8346a7df19ad2279b96699
mtenyg24 <m.gatluak@alustudent.com>
2024-05-22 15:09:58 +0300

    import module

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -2,7 +2,7 @@
 """ Forward Prop
 """
 import tensorflow as tf
-
+create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """ Function that builds the forward propagation graph for
