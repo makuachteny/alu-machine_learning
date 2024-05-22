correct units

supervised_learning/tensorflow/1-create_layer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
-""" create layer """
+"""Module defines a function that creates a layer"""
+
 import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-    """ create layer"""
-    W = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    L = tf.layers.Dense(units=n, activation=activation,
-                        kernel_initializer=W, name="layer")
-    return L(prev)
+    """Creates a dense layer using previous layer."""
+    # Initialize weights using He et al. initializer
+    W_init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
+    # Create the dense layer with activation function
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=W_init, name='layer')
+
+    return layer(prev)