5fa189f06329054be0e408c9d10b95152d610650
mtenyg24 <m.gatluak@alustudent.com>
2024-05-28 10:04:18 +0300

    Pycodestyle validation

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,12 +7,12 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """ L2 Regularization layer"""
 
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    init = tf.contrib.layers.variance_scaling_init(mode="FAN_AVG")
 
     l2 = tf.contrib.layers.l2_regularizer(lambtha)
 
     layer = tf.layers.Dense(n, activation=activation,
-                            kernel_initializer=initializer,
+                            kernel_init=init,
                             kernel_regularizer=l2)
 
     return layer(prev)
