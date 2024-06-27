4422e9220a1fd2164cfce64d74d67d0224039320
mtenyg24 <m.gatluak@alustudent.com>
2024-06-27 13:09:01 +0300

    Grant permission

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,25 +1,51 @@
 #!/usr/bin/env python3
 
-
-""" Return list of ships"""
+"""
+Script to print the location of a specific GitHub user.
+"""
 
 import requests
 import sys
 import time
 
 
+def get_user_location(api_url):
+    """
+    Fetches and prints the location of a GitHub user based on the provided API URL.
+
+    Args:
+        api_url (str): The full API URL for the GitHub user.
+
+    Prints:
+        - The location of the user if available.
+        - "Reset in X min" if rate limit exceeded.
+        - "Not found" if the user does not exist.
+        - Error messages for other HTTP status codes or request exceptions.
+    """
+    try:
+        response = requests.get(api_url)
+
+        if response.status_code == 200:
+            user_data = response.json()
+            location = user_data.get("location", "Location not available")
+            print(location)
+        elif response.status_code == 404:
+            print("Not found")
+        elif response.status_code == 403:
+            rate_limit_reset = int(
+                response.headers.get('X-Ratelimit-Reset', 0))
+            current_time = int(time.time())
+            diff = (rate_limit_reset - current_time) // 60
+            print(f"Reset in {diff} min")
+        else:
+            print(f"Error: {response.status_code}")
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+
+
 if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
-
-    if res.status_code == 403:
-        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
-        current_time = int(time.time())
-        diff = (rate_limit - current_time) // 60
-        print("Reset in {} min".format(diff))
-        # get remaining rate
-
-    elif res.status_code == 404:
-        print("Not found")
-    elif res.status_code == 200:
-        res = res.json()
-        print(res['location'])
+    if len(sys.argv) != 2:
+        print("Usage: ./2-user_location.py <GitHub API URL>")
+    else:
+        api_url = sys.argv[1]
+        get_user_location(api_url)
