58992186c73c0880e711cd5af6098f8b8e14f11c
makuachteny <m.gatluak@alustudent.com>
2024-07-04 10:27:31 +0000

    Genre ID by show

---
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- Script lists all shows contained in hbtn_0d_tvshows that have atleast one genre linked.
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM hbtn_0d_tvshows.tv_shows
+LEFT JOIN hbtn_0d_tvshows.tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id
