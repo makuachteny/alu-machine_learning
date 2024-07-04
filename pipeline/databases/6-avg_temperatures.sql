e24a14122e1faa3f869ed89e5206c178a817a4a6
mtenyg24 <m.gatluak@alustudent.com>
2024-07-04 12:27:34 +0300

    Average temperature

---
+++ pipeline/databases/6-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Import the database dump from hbtn_0c_0 and display the average temperature (Fahrenheit) by city ordered by temperature (descending).
+
+SELECT city, AVG(temperature) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
