2688906b3d366a8452ed678c8215c6481ab2d327
mtenyg24 <m.gatluak@alustudent.com>
2024-07-08 21:41:24 +0300

    Inner join ratings

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -3,5 +3,6 @@
 SELECT tv_genres.name, SUM(rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_ratings.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
