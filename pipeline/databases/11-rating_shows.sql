397007db2b77f0ea6ee4472169af36095e2490b4
mtenyg24 <m.gatluak@alustudent.com>
2024-07-08 21:20:18 +0300

    Sum rating

---
+++ pipeline/databases/11-rating_shows.sql
@@ -0,0 +1,3 @@
+-- Script lists all the shows by their rating
+
+SELECT tv_shows.title, SUM(rate) as rating sum FROM tv_shows LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id GROUP BY tv_shows.title ORDER BY rating DESC
