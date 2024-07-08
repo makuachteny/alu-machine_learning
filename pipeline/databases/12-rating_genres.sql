7970c0def8434f81eadb55ed415e14b659d6eeb4
mtenyg24 <m.gatluak@alustudent.com>
2024-07-08 22:11:27 +0300

    Correct syntax

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -1,7 +1,7 @@
 -- Script lists all genres in the database hbtn_0d_tvshows database by their rating
 
-SELECT tv_genres.name, SUM(rate) AS rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating_sum
 FROM tv_genres
-LEFT JOIN tv_show_ratings ON tv_genres.id = tv_show_ratings.genre_id
+LEFT JOIN tv_show_ratings ON tv_genres.id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
-ORDER BY rating DESC;
+ORDER BY rating_sum DESC;
