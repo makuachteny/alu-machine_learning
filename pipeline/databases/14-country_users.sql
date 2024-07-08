ddf5b6e7b6d7ebaaf761093ef18d4843fc1d3a1e
mtenyg24 <m.gatluak@alustudent.com>
2024-07-08 23:15:28 +0300

    Task 14 added

---
+++ pipeline/databases/14-country_users.sql
@@ -0,0 +1,8 @@
+-- Script creates a table users with attributes id, email, name and country
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255) NOT NULL,
+    country ENUM('US', 'CO', 'TN') NOT NULL
+);
