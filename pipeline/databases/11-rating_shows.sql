19d590625d8573a0748838f693e70b72298216d5
mtenyg24 <m.gatluak@alustudent.com>
2024-07-08 21:27:36 +0300

    Group desc order

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,3 +1,6 @@
 -- Script lists all the shows by their rating
 
-SELECT tv_shows.title, SUM(rate) AS ratingsum FROM tv_shows LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id GROUP BY tv_shows.title ORDER BY ratingsum DESC
+SELECT tv_shows.title, SUM(rate) AS rating
+FROM tv_shows
+LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+ORDER BY rating DESC;
