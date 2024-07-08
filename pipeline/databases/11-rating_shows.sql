a62a672447887b110dc6998194acebc543cf420c
mtenyg24 <m.gatluak@alustudent.com>
2024-07-08 21:24:23 +0300

    Keywords in Capital

--- pipeline/databases/11-rating_shows.sql
+++ pipeline/databases/11-rating_shows.sql
@@ -1,3 +1,3 @@
 -- Script lists all the shows by their rating
 
-SELECT tv_shows.title, SUM(rate) as ratingsum FROM tv_shows LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id GROUP BY tv_shows.title ORDER BY ratingsum DESC
+SELECT tv_shows.title, SUM(rate) AS ratingsum FROM tv_shows LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id GROUP BY tv_shows.title ORDER BY ratingsum DESC
