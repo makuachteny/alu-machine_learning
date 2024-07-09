cec85906f224abf0f3dc120247fe8254c9254259
mtenyg24 <m.gatluak@alustudent.com>
2024-07-09 13:14:30 +0300

    Drop previous table

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -6,7 +6,8 @@
 -- email
 -- name
 -- country(enums: 'US', "CO", 'TN') - not null, default 'US'
-CREATE Table IF NOT EXISTS users(
+DROP TABLE IF EXISTS users;
+CREATE TABLE IF NOT EXISTS users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255),
