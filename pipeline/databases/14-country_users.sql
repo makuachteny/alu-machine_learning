75a0db26d342b067c26d5f391f78811ad2078cf3
mtenyg24 <m.gatluak@alustudent.com>
2024-07-09 13:09:56 +0300

    Add countries to users

--- pipeline/databases/14-country_users.sql
+++ pipeline/databases/14-country_users.sql
@@ -1,8 +1,15 @@
 -- Script creates a table users with attributes id, email, name and country
 
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255) NOT NULL,
-    country ENUM('US', 'CO', 'TN') NOT NULL
+-- in table users
+-- -- insert:
+-- id 
+-- email
+-- name
+-- country(enums: 'US', "CO", 'TN') - not null, default 'US'
+CREATE Table IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    UNIQUE (email)
 );
