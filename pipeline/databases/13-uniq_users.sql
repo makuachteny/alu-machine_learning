ada8590b226de304bffec1efb78bbbdad13fe973
mtenyg24 <m.gatluak@alustudent.com>
2024-07-09 13:18:43 +0300

    Drop initial table

--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -1,5 +1,5 @@
 -- Script that creates a table `users`
-
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
