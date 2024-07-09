d46102fae38e3035663d300b83f0264ed8213b39
mtenyg24 <m.gatluak@alustudent.com>
2024-07-09 13:21:02 +0300

    Fix syntax

--- pipeline/databases/13-uniq_users.sql
+++ pipeline/databases/13-uniq_users.sql
@@ -1,5 +1,4 @@
 -- Script that creates a table `users`
-DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
