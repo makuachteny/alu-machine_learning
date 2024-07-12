5e30462c702390e98079da50351290baa875aa14
mtenyg24 <m.gatluak@alustudent.com>
2024-07-12 20:55:16 +0300

    Refactor code

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,13 +1,23 @@
 -- Create a stored procedure to compute the average score
 -- for a user and update the average_score column in the users table.
-DELIMITER // CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id_param INT) BEGIN
-DECLARE user_avg_score FLOAT;
+DELIMITER // 
+
+CREATE PROCEDURE ComputeAverageScoreForUser (
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+
 -- Calculate average score for the user
-SELECT AVG(score) INTO user_avg_score
+SELECT AVG(score) INTO avg_score
 FROM corrections
-WHERE user_id = user_id_param;
+WHERE user_id = user_id;
 -- Update the average_score column in the users table
 UPDATE users
-SET average_score = user_avg_score
-WHERE id = user_id_param;
-END // DELIMITER;
+SET average_score = avg_score
+WHERE id = user_id;
+END;
+
+//
+
+DELIMITER;
