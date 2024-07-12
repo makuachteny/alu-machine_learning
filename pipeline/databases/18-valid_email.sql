1443f0e85f983cd265cc3324d656b3ae74485685
mtenyg24 <m.gatluak@alustudent.com>
2024-07-12 20:46:50 +0300

    Use // instead of $ constructs

--- pipeline/databases/18-valid_email.sql
+++ pipeline/databases/18-valid_email.sql
@@ -1,6 +1,15 @@
 -- Validate email
-DELIMITER $$ CREATE TRIGGER reset_valid_email BEFORE
-UPDATE ON users FOR EACH ROW BEGIN IF STRCMP(old.email, new.email) <> 0 THEN
-SET new.valid_email = 0;
+DELIMITER //
+
+CREATE TRIGGER email_update_trigger BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN 
+    -- Check if the email has changed
+    IF STRCMP(old.email, new.email) <> 0 THEN
+    -- Reset the valid_email flag
+    SET new.valid_email = 0;
 END IF;
-END $$
+END;
+
+//
+DELIMITER ;
--- pipeline/databases/31-insert_school.py
+++ pipeline/databases/31-insert_school.py
@@ -1,25 +1,5 @@
 #!/usr/bin/env python3
-""" 
-This script is used to insert a new school into a MongoDB collection and print the details of all schools in the collection.
-
-Usage:
-    python 31-insert_school.py
-
-Dependencies:
-    - pymongo
-
-Functions:
-    - insert_school(collection, name, address): Inserts a new school document into the specified collection with the given name and address.
-    - list_all(collection): Retrieves all school documents from the specified collection.
-
-Example:
-    $ python 31-insert_school.py
-    New school created: 1234567890
-    [1234567890] UCSF 505 Parnassus Ave
-    [0987654321] Stanford University 450 Serra Mall
-    ...
-"""
-
+""" 31-main """
 from pymongo import MongoClient
 list_all = __import__('30-all').list_all
 insert_school = __import__('31-insert_school').insert_school
