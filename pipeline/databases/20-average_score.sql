25c7c638f59eccad605b003e61419955902974cf
mtenyg24 <m.gatluak@alustudent.com>
2024-07-12 20:58:37 +0300

    Hot fix

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,23 +1,12 @@
--- Create a stored procedure to compute the average score
--- for a user and update the average_score column in the users table.
-DELIMITER // 
-
-CREATE PROCEDURE ComputeAverageScoreForUser (
-    IN user_id INT
-)
-BEGIN
-    DECLARE avg_score FLOAT;
-
--- Calculate average score for the user
+DELIMITER // CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
+DECLARE avg_score DECIMAL(5, 2);
+-- Compute the average score
 SELECT AVG(score) INTO avg_score
 FROM corrections
 WHERE user_id = user_id;
--- Update the average_score column in the users table
+-- Update the users table with the computed average score
 UPDATE users
 SET average_score = avg_score
 WHERE id = user_id;
 END;
-
-//
-
-DELIMITER;
+// DELIMITER;
