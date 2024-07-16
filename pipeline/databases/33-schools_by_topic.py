03582b4562bfca755fc820b7ed6b9bae3e52d1cb
mtenyg24 <m.gatluak@alustudent.com>
2024-07-16 10:57:14 +0300

    List schools with specific topic

--- pipeline/databases/33-schools_by_topic.py
+++ pipeline/databases/33-schools_by_topic.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
-""" 33-main """
-from pymongo import MongoClient
-list_all = __import__('30-all').list_all
-insert_school = __import__('31-insert_school').insert_school
-schools_by_topic = __import__('33-schools_by_topic').schools_by_topic
+"""
+Script that returns the list of schools with a specific topic
+"""
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
 
-    j_schools = [
-        {'name': "Holberton school", 'topics': [
-            "Algo", "C", "Python", "React"]},
-        {'name': "UCSF", 'topics': ["Algo", "MongoDB"]},
-        {'name': "UCLA", 'topics': ["C", "Python"]},
-        {'name': "UCSD", 'topics': ["Cassandra"]},
-        {'name': "Stanford", 'topics': ["C", "React", "Javascript"]}
-    ]
-    for j_school in j_schools:
-        insert_school(school_collection, **j_school)
+def schools_by_topic(mongo_collection, topic):
+    """
+    Finds list of all schools with a specific topic
 
-    schools = schools_by_topic(school_collection, "Python")
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'),
-              school.get('name'), school.get('topics', "")))
+    parameters:
+        mongo_collection [pymongo]:
+            the MongoDB collection to use
+        topic [string]:
+            the topic to search for
+
+    returns:
+        list of schools with the given topic
+    """
+    schools = []
+    documents = mongo_collection.find({'topics': {'$all': [topic]}})
+    for doc in documents:
+        schools.append(doc)
+    return schools
