7ebaa7ff4509ab489c87e994c7a1f4b5a2753178
mtenyg24 <m.gatluak@alustudent.com>
2024-07-16 10:47:31 +0300

    Reset delimeter

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -1,12 +1,13 @@
-DELIMITER // CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT) BEGIN
-DECLARE avg_score DECIMAL(5, 2);
--- Compute the average score
-SELECT AVG(score) INTO avg_score
-FROM corrections
-WHERE user_id = user_id;
--- Update the users table with the computed average score
+
+-- create a stored procedure
+DELIMITER $$ CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id_new INTEGER) BEGIN
 UPDATE users
-SET average_score = avg_score
-WHERE id = user_id;
+SET average_score =(
+        SELECT AVG(score)
+        FROM corrections
+        WHERE user_id = user_id_new
+    )
+WHERE id = user_id_new;
 END;
-// DELIMITER;
+$$ -- Reset delimiter
+DELIMITER;
