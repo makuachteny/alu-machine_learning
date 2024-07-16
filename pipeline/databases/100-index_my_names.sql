39babe3d0230f2427b3738a4a97d24014744f44a
mtenyg24 <m.gatluak@alustudent.com>
2024-07-16 11:12:17 +0300

    Drop existing index

--- pipeline/databases/100-index_my_names.sql
+++ pipeline/databases/100-index_my_names.sql
@@ -1,5 +1,4 @@
--- Import the table dump
--- Ensure to unzip names.sql.zip and use the following command in your MySQL environment:
--- mysql -u your_username -p your_database < path_to_unzipped_names.sql
+-- Drop the existing index if it exists
+DROP INDEX IF EXISTS idx_name_first ON names;
 -- Create an index on the first letter of the name column
 CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
