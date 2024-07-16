5ff4bfb28362732c1d5a87ce2faf8e1596286706
mtenyg24 <m.gatluak@alustudent.com>
2024-07-16 10:55:25 +0300

    Update topics

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
-""" 32-main """
-from pymongo import MongoClient
-list_all = __import__('30-all').list_all
-update_topics = __import__('32-update_topics').update_topics
+"""
+Defines function that changes all topics of a school document based on name
+"""
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    update_topics(school_collection, "Holberton school",
-                  ["Sys admin", "AI", "Algorithm"])
 
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'),
-              school.get('name'), school.get('topics', "")))
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
 
-    update_topics(school_collection, "Holberton school", ["iOS"])
-
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'),
-              school.get('name'), school.get('topics', "")))
+    parameters:
+        mongo_collection [pymongo]:
+            the MongoDB collection to use
+        name [string]:
+            the school name to update
+        topics [list of strings]:
+            list of topics approached in the school
+    """
+    mongo_collection.update_many({'name': name},
+                                 {'$set': {'topics': topics}})
