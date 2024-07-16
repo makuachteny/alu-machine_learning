ac1485820a42848713a6631796d12fef7b721a99
mtenyg24 <m.gatluak@alustudent.com>
2024-07-16 10:49:58 +0300

    Correct syntax

--- pipeline/databases/21-div.sql
+++ pipeline/databases/21-div.sql
@@ -1,5 +1,6 @@
 -- Content: SQL to create a function that safely divides two numbers
-DELIMITER $$ CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT BEGIN IF b = 0 THEN RETURN 0;
-ELSE RETURN a / b;
+-- safe div
+DELIMITER $$ CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS FLOAT DETERMINISTIC BEGIN IF b = 0 THEN RETURN 0;
+ELSE RETURN (a / b);
 END IF;
-END $$
+END $$ DELIMITER;
