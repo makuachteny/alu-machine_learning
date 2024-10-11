e8678c1a7e6e4ae2f800ffe5a653a82bf096b15c
makuachteny <m.gatluak@alustudent.com>
2024-10-11 08:56:40 -0400

    Positional Encoding

---
+++ supervised_learning/attention/4-positional_encoding.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+module with a function that calculates
+the positional encoding for a transformer
+"""
+import numpy as np
+
+
+def get_angle(pos, i, dm):
+    """
+    formulas for positional encoding:
+    PE(pos, 2i) = sin(pos / 10000^(2i / dm))
+    PE(pos, 2i + 1) = cos(pos / 10000^(2i / dm))
+    """
+    angle_rates = 1 / (10000 ** (i / dm))
+    return pos * angle_rates
+
+
+def positional_encoding(max_seq_len, dm):
+    """
+    Calculates the positional encoding for a transformer
+    max_seq_len [int]- maximum sequence length
+    dm: model depth
+    returns:
+    [numpy.ndarray- shape (max_seq_len, dm)] - positional
+    encoding vectors
+    """
+    positional_encoding = np.zeros([max_seq_len, dm])
+
+    for pos in range(max_seq_len):
+        for i in range(0, dm, 2):
+            # sin for even indices of positional_encoding
+            positional_encoding[pos, i] = np.sin(get_angle(pos, i, dm))
+            # cos for odd indices of positional_encoding
+            positional_encoding[pos, i + 1] = np.cos(get_angle(pos, i, dm))
+    return positional_encoding
