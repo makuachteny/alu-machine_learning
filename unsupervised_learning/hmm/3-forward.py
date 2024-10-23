8b77c4d94855c58e81fe65fa4c1f47f8c1656c18
makuachteny <m.gatluak@alustudent.com>
2024-10-22 22:59:39 -0400

    correct output

--- unsupervised_learning/hmm/3-forward.py
+++ unsupervised_learning/hmm/3-forward.py
@@ -1,64 +1,63 @@
 #!/usr/bin/env python3
-"""
-Defines function that performs the forward algorithm for a Hidden Markov Model
-"""
 
+"""
+This module initializes forward algorithm
+for a hidden markov model
+"""
 
 import numpy as np
 
 
 def forward(Observation, Emission, Transition, Initial):
     """
-    Performs the forward algorithm for a Hidden Markov Model
+    initializes forward algorithm for a hidden
+    markov model
 
-    parameters:
-        Observation [numpy.ndarray of shape (T,)]:
-            contains the index of the observation
-            T: number of observations
-        Emission [numpy.ndarray of shape (N, M)]:
-            contains the emission probability of a specific observation
-                given a hidden state
-            N: number of hidden states
-            M: number of all possible observations
-        Transition [2D numpy.ndarray of shape (N, N)]:
-            contains the transition probabilities
-            Transition[i, j] is the probabilitiy of transitioning
-                from the hidden state i to j
-        Initial [numpy.ndarray of shape (N, 1)]:
-            contains the probability of starting in a particular hidden state
-
-    returns:
-        P, F:
-            P [float]:
-                the likelihood of the observations given the model
-            F [a numpy.ndarray of shape (N. T)]:
-                contains the forward path probabilities
-                F[i, j] is the probability of being in hidden state i at time j
-                    given the previous observations
-        or None, None on failure
+    Observation - numpy.ndarray (T,) that contains
+    index of the observation
+        - T - number of observations
+    Emission - numpy.ndarray (N, M) containing the
+    emission probability of a specific observation
+        given a hidden state
+        - Emission[i, j] is the probability of observing
+        j given the hidden state i
+        - N is the number of hidden states
+        - M is the number of all possible observations
+    Transition - 2D numpy.ndarray (N, N) containing the
+    transition probabilities
+        - Transition[i, j] is the probability of transitioning
+        from the hidden state i to j
+    Initial - numpy.ndarray (N, 1) containing the probability
+    of starting in a particular hidden state
+    Return:
+    P, F, or None, None on failure
+        - P is the likelihood of the observations given the model
+        - F is a numpy.ndarray (N, T) containing the forward path
+        probabilities
+        - F[i, j] is the probability of being in hidden state i
+        after observing the first j observations
     """
-    # check that Observation is the correct type and dimension
-    if type(Observation) is not np.ndarray or len(Observation.shape) < 1:
+    if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
         return None, None
-    # save T from Observation's shape
     T = Observation.shape[0]
-    # check that Emission is the correct type and dimension
     if type(Emission) is not np.ndarray or len(Emission.shape) != 2:
         return None, None
-    # save N and M from Emission's shape
     N, M = Emission.shape
-    # check that Transition is the correct type and dimension
     if type(Transition) is not np.ndarray or len(Transition.shape) != 2:
         return None, None
-    # check that Transition's dimensions match N from Emission
-    N_check1, N_check2 = Transition.shape
-    if N_check1 != N or N_check2 != N:
+    N1, N2 = Transition.shape
+    if N1 != N or N2 != N:
         return None, None
-    # check that Initial is the correct type and dimension
     if type(Initial) is not np.ndarray or len(Initial.shape) != 2:
         return None, None
-    # check that Initial's dimensions match (N, 1)
-    N_check1, one = Initial.shape
-    if N_check1 != N or one != 1:
+    N3, N4 = Initial.shape
+    if N3 != N or N4 != 1:
         return None, None
-    return None, None
+    F = np.zeros((N, T))
+    F[:, 0] = Initial.T * Emission[:, Observation[0]]
+    for i in range(1, T):
+        F[:, i] = np.sum(
+            F[:, i - 1] * Transition.T * Emission[np.newaxis, :,
+                                                  Observation[i]].T, axis=1)
+    P = np.sum(F[:, -1])
+    return P, F
