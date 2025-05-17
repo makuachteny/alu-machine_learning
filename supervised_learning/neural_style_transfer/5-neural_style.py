ee9671ea4db51abac2399309d8ce92793523b6c0
m.gatluak@alustudent.com <m.gatluak@alustudent.com>
2025-05-17 13:58:55 -0400

    combine model outputs

--- supervised_learning/neural_style_transfer/5-neural_style.py
+++ supervised_learning/neural_style_transfer/5-neural_style.py
@@ -138,35 +138,26 @@ class NST:
     def load_model(self):
         """
         Creates the model used to calculate cost from VGG19 Keras base model
-
-        Model's input should match VGG19 input
-        Model's output should be a list containing outputs of VGG19 layers
-            listed in style_layers followed by content_layers
-
-        Saves the model in the instance attribute model
         """
         VGG19_model = tf.keras.applications.VGG19(include_top=False,
                                                   weights='imagenet')
-        VGG19_model.save("VGG19_base_model")
-        custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
 
-        vgg = tf.keras.models.load_model("VGG19_base_model",
-                                         custom_objects=custom_objects)
+        # Extract outputs for style and content layers
+        style_outputs = [VGG19_model.get_layer(name).output
+                         for name in self.style_layers]
+        content_output = VGG19_model.get_layer(self.content_layer).output
 
-        style_outputs = []
-        content_output = None
-
-        for layer in vgg.layers:
-            if layer.name in self.style_layers:
-                style_outputs.append(layer.output)
-            if layer.name in self.content_layer:
-                content_output = layer.output
-
-            layer.trainable = False
-
+        # Combine style and content outputs
         outputs = style_outputs + [content_output]
 
-        model = tf.keras.models.Model(vgg.input, outputs)
+        # Create the model
+        model = tf.keras.models.Model(
+            inputs=VGG19_model.input, outputs=outputs)
+
+        # Freeze the model layers
+        for layer in model.layers:
+            layer.trainable = False
+
         self.model = model
 
     @staticmethod
