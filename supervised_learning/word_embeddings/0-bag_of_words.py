85c2fe0892c2bcbeffdd3b82593f0f117e26d945
m.gatluak@alustudent.com <m.gatluak@alustudent.com>
2025-05-28 12:25:21 -0400

    correct embeddings

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,48 +1,43 @@
 #!/usr/bin/env python3
 
-"""This module has a function that
-creates a bag of words embedding matrix"""
-# from sklearn.feature_extraction.text import CountVectorizer
+"""This module has a function that creates a bag of words embedding matrix"""
 import numpy as np
-from collections import Counter
 import re
 
 
 def bag_of_words(sentences, vocab=None):
-    """creates a bag of words embedding matrix
-    sentence - list of sentences to analyze
-    vocab - list of vocab words for analysis
-    returns embeddings, features
-    embeddings shape - (s, f)
-        s-no. of sentences in sentences
-        f-mo. of features analyzed
-    features - list of features for embeddings
     """
+    Creates a bag of words embedding matrix.
 
+    Parameters:
+    sentences (list): List of sentences to analyze.
+    vocab (list): List of vocabulary words to use for the analysis. 
+    If None, all words within sentences are used.
+
+    Returns:
+    tuple: embeddings (numpy.ndarray), features (list)
+        embeddings shape - (s, f)
+            s - number of sentences in sentences
+            f - number of features analyzed
+        features - list of features used for embeddings
+    """
+    # Tokenize sentences and remove punctuation
     tokenized_sentences = []
-    # Tokenize sentences
     for sentence in sentences:
-        # remove punctuation and split into words
-        sentence = re.sub(r'[^\w\s]', '', sentence)
-        words = sentence.lower().split()
+        sentence = re.sub(r'[^\w\s]', '', sentence)  # remove punctuation
+        words = sentence.lower().split()  # lowercase
         tokenized_sentences.append(words)
 
-    # if vocab is none, all words in sentences are used
+    # If vocab is None, use all unique words in sentences
     if vocab is None:
-        # create a set of all unique words in sentences
-        vocab = set()
-        # add all words to vocab
-        for sentence in tokenized_sentences:
-            vocab.update(sentence)
+        vocab = sorted(
+            set(word for sentence in tokenized_sentences for word in sentence))
 
-    # create embeddings matrix
+    # Create embeddings matrix
     embeddings = np.zeros((len(sentences), len(vocab)))
     for i, sentence in enumerate(tokenized_sentences):
-        # count words in sentence
-        word_counts = Counter(sentence)
-        for j, word in enumerate(vocab):
-            # if word is in sentence add its count to embeddings
-            if word in word_counts:
-                embeddings[i, j] = word_counts[word]
-    
-    return embeddings, list(vocab)
+        for word in sentence:
+            if word in vocab:
+                embeddings[i, vocab.index(word)] += 1
+
+    return embeddings, vocab
